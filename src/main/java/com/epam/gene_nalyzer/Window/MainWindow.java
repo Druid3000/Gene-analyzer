7fe5997ac972ea118e132c30117c53461ff8f88b
Anna Timoshkina <timoshkinaanna83@gmail.com>
2019-02-25 02:12:17 +0300

    Apply IDEA auto recommendation for MainWindow class.

--- src/main/java/com/epam/gene_nalyzer/Window/MainWindow.java
+++ src/main/java/com/epam/gene_nalyzer/Window/MainWindow.java
@@ -8,15 +8,14 @@ import java.awt.*;
 
 public class MainWindow extends JFrame {
 
-    private MainController mainController = new MainController();
-    private GraphWindow graphWindow = new GraphWindow(mainController);
-    private AboutWindow aboutWindow = new AboutWindow();
-    private TableWindow tableWindow = new TableWindow(mainController);
-    private CanvasLine canvasLine = new CanvasLine(mainController);
-
-
     public MainWindow() {
+        MainController mainController = new MainController();
+        GraphWindow graphWindow = new GraphWindow(mainController);
+        AboutWindow aboutWindow = new AboutWindow();
+        TableWindow tableWindow = new TableWindow(mainController);
+        CanvasLine canvasLine = new CanvasLine(mainController);
         Menu menu = new Menu(aboutWindow, tableWindow, mainController, graphWindow, canvasLine);
+
         setVisible(true);
         setTitle("Определение оптической плотности");
         setPreferredSize(new Dimension(1000, 1000));
