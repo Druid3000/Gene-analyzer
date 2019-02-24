3c0534b036fc31e80c3e8dfac1243b93687ec21c
Anna Timoshkina <timoshkinaanna83@gmail.com>
2019-02-25 01:59:45 +0300

    Apply IDEA auto recommendation for GraphWindow class.

--- src/main/java/com/epam/gene_nalyzer/Window/GraphWindow.java
+++ src/main/java/com/epam/gene_nalyzer/Window/GraphWindow.java
@@ -5,15 +5,15 @@ import com.epam.gene_nalyzer.Controllers.MainController;
 import javax.swing.*;
 
 public class GraphWindow extends JFrame {
-    private MainController mainController;
-    private CanvasGraph canvasGraph;
 
     public GraphWindow(MainController mc) {
+
+        MainController mainController = mc;
+        CanvasGraph canvasGraph = new CanvasGraph(mainController);
         setTitle("Графики оптической плотности");
-        mainController = mc;
-        canvasGraph = new CanvasGraph(mainController);
         add(canvasGraph);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(550, 550);
+
     }
 }
