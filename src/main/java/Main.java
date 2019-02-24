cdbda75e942f2e46fcbefab127ff6154b42e910b
IceVenOm98 <IceVenOm98@gmail.com>
2019-02-24 16:25:41 +0300

    Main.java moved to the root.

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Main {
-    public static void main(String[] args) {
-        com.epam.gene_nalyzer.Window.MainWindow mainWindow = new com.epam.gene_nalyzer.Window.MainWindow();
-
-    }
-}
---
+++ src/main/java/com/epam/gene_nalyzer/Main.java
@@ -0,0 +1,10 @@
+package com.epam.gene_nalyzer;
+
+import com.epam.gene_nalyzer.Window.MainWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+
+    }
+}
