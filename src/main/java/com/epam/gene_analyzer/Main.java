ce4389d82b65d82794f22697fca4517a4c6c3bd2
Druid3000 <sarvarov.s@mail.ru>
2019-02-25 18:38:14 +0300

    Delete a empty lines in class Main.java

--- src/main/java/com/epam/gene_analyzer/Main.java
+++ src/main/java/com/epam/gene_analyzer/Main.java
@@ -2,7 +2,6 @@ package com.epam.gene_analyzer;
 
 import com.epam.gene_analyzer.window.MainWindow;
 
-
 public class Main {
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
