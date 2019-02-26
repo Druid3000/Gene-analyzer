c10bf953421c0f4ed7b00054354828fcbda14c34
Sergei Sarvarov <sergei_sarvarov@epam.com>
2019-02-26 23:11:47 +0300

    Add build block to pon.xml also change modifier of class AboutWindow to package-private

--- src/main/java/com/epam/gene_analyzer/windows/AboutWindow.java
+++ src/main/java/com/epam/gene_analyzer/windows/AboutWindow.java
@@ -2,7 +2,7 @@ package com.epam.gene_analyzer.windows;
 
 import javax.swing.*;
 
-public class AboutWindow extends JFrame {
+class AboutWindow extends JFrame {
     AboutWindow() {
         super("About Program");
         setDefaultCloseOperation(HIDE_ON_CLOSE);
