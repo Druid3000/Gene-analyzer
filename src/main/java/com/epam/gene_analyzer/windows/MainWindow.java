cdbed59dfa8a98973b7f1dd793423ec6441e34c6
Александра <warmalex@yandex.ru>
2019-02-26 20:31:18 +0300

    translate title, some comments into english

--- src/main/java/com/epam/gene_analyzer/windows/MainWindow.java
+++ src/main/java/com/epam/gene_analyzer/windows/MainWindow.java
@@ -22,7 +22,7 @@ public class MainWindow extends JFrame {
 
     public void showWindow(){
         setVisible(true);
-        setTitle("Определение оптической плотности");
+        setTitle("Optical density determination");
         setPreferredSize(new Dimension(1000, 1000));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setJMenuBar(menu.getMenuBar());
