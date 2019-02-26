5925198f6f3e540d6eaeff5581e743f8136d08d8
Sergei Sarvarov <sergei_sarvarov@epam.com>
2019-02-26 22:49:41 +0300

    Refactor of class Square

--- src/main/java/com/epam/gene_analyzer/model/Square.java
+++ src/main/java/com/epam/gene_analyzer/model/Square.java
@@ -8,23 +8,33 @@ public class Square {
     private ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 
     public Square(Pixel p1, Pixel p2, BufferedImage image){
-        int x1 = p1.getX(), x2 = p2.getX(), y1 = p1.getY(), y2 = p2.getY();
+        int x1 = p1.getX();
+        int x2 = p2.getX();
+        int y1 = p1.getY();
+        int y2 = p2.getY();
+
         Pixel[][] allPixels;
-        int h = image.getHeight(), w = image.getWidth(), R, G, B;
+        int h = image.getHeight();
+        int w = image.getWidth();
+
+        int r;
+        int g;
+        int b;
+
         allPixels = new Pixel[w][h];
         Color c;
         for (int i = 0; i < w; i++) {
             for (int j = 0; j < h; j++) {
                 c = new Color(image.getRGB(i, j));
-                R = c.getRed();
-                G = c.getGreen();
-                B = c.getBlue();
+                r = c.getRed();
+                g = c.getGreen();
+                b = c.getBlue();
                 Pixel p = new Pixel();
                 p.setX(i);
                 p.setY(j);
-                p.setR(R);
-                p.setG(G);
-                p.setB(B);
+                p.setR(r);
+                p.setG(g);
+                p.setB(b);
                 allPixels[i][j] = p;
             }
         }
