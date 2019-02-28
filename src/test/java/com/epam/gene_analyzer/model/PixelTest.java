e572ad89a3d5e5983c91484adfce78325ae564f7
Anna Timoshkina <timoshkinaanna83@gmail.com>
2019-02-28 18:05:47 +0300

    Add test for Pixel class (+3 methods)

--- src/test/java/com/epam/gene_analyzer/model/PixelTest.java
+++ src/test/java/com/epam/gene_analyzer/model/PixelTest.java
@@ -3,18 +3,20 @@ package com.epam.gene_analyzer.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class PixelTest {
 
     private Pixel pixel;
     private int[] sourceRGB = { 1, 2, 3 };
-    private double expected;
+    private double expectedIntensity;
 
     @Before
     public void initTest() {
         pixel = new Pixel();
-        expected = 1.81;
+        expectedIntensity = 1.81;
         pixel.setR(sourceRGB[0]);
         pixel.setG(sourceRGB[1]);
         pixel.setB(sourceRGB[2]);
@@ -23,6 +25,24 @@ public class PixelTest {
     @Test
     public void testGetIntensity() {
         double actual = pixel.getIntensity();
-        assertEquals((long) expected, (long) actual);
+        assertEquals((long) expectedIntensity, (long) actual);
+    }
+
+    @Test
+    public void testSetX(){
+        pixel.setX(1);
+        assertEquals(1,pixel.getX());
+    }
+
+    @Test
+    public void testSetY(){
+        pixel.setY(1);
+        assertEquals(1,pixel.getY());
+    }
+
+    @Test
+    public void  testSetColor(){
+        pixel.setColor(new Color(10,20,30));
+        assertEquals(new Color(10,20,30),pixel.getColor());
     }
 }
