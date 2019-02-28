80f43b244dca72e8485559591132b4d5f7c838c6
alisagareeva19 <alisagareeva19@gmail.com>
2019-02-28 05:11:26 +0300

    Add JavaDoc to controllers

--- src/main/java/com/epam/gene_analyzer/controllers/MainController.java
+++ src/main/java/com/epam/gene_analyzer/controllers/MainController.java
@@ -9,35 +9,61 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/** Controller class for creating graphics and tables.
+ * Using array of lines, array of pixels for drawing graphics, array of areas, buffered pictures, values of background intensity, table of areas.
+ *
+ */
 public class MainController {
-    private ArrayList<Line> lineArray = new ArrayList<Line>();//массив всех линий
-    private ArrayList<Pixel> graphPixels = new ArrayList<Pixel>();//массив пикселей для отрисовки графиков
-    private ArrayList<Area> areaArray = new ArrayList<Area>();//массив всех областей
+    private ArrayList<Line> lineArray = new ArrayList<Line>();
+    private ArrayList<Pixel> graphPixels = new ArrayList<Pixel>();
+    private ArrayList<Area> areaArray = new ArrayList<Area>();
     private BufferedImage picture;
     private int numberOfLinesOld = 0;
     private double backgroundIntensityOld = 255.0;
     private double backgroundIntensity = 255.0;
     private double delta = 0.1;
-    private RtModel rtModel = new RtModel();//таблица областей
+    private RtModel rtModel = new RtModel();
 
+    /** Method for setting value of delta
+     *
+     * @param d value of possible distance difference between pixels
+     */
     public void setDelta(double d) {
         delta = d;
     }
 
+    /** Method for adding object of class Line in lineArray
+     *
+     * @param p1 value of pixel
+     * @param p2 value of pixel
+     */
     public void addLine(Pixel p1, Pixel p2) {
         Line l = new Line(p1, p2, picture);
         lineArray.add(l);
 
     }
 
+    /** Method for get id of line
+     *
+     * @param id of line
+     * @return array of line id
+     */
     public Line getLine(int id) {
         return lineArray.get(id);
     }
 
+    /** Method for buffering picture from computer
+     *
+     * @return buffered picture
+     */
     public BufferedImage getPicture() {
         return picture;
     }
 
+    /** Method for setting file of picture
+     *
+     * @param f file of picture
+     */
     public void setPicture(File f) {
         try {
             picture = ImageIO.read(f);
@@ -46,11 +72,23 @@ public class MainController {
         }
     }
 
+    /** Method for getting list of line by using array of pixels
+     *
+     * @return array of pixels
+     */
     public ArrayList<Line> getLines() {
         if (lineArray != null) return lineArray;
         else return null;
     }
 
+    /** Method for adding area.
+     * Create new area.
+     * Set new area on picture with using delta.
+     * Add new area to array of areas.
+     * Update data about areas.
+     *
+     * @param p chosen pixel on the picture
+     */
     public void addArea(Pixel p) {
         Area area = new Area();
         area.setArea(p, picture, delta);
@@ -58,21 +96,39 @@ public class MainController {
         updateData();
     }
 
+    /** Method for getting area by area id
+     *
+     * @param id number of area
+     * @return array of areas
+     */
     public Area getArea(int id) {
         return areaArray.get(id);
     }
 
+    /** Method for getting list of area
+     *
+     * @return array of areas
+     */
     public ArrayList<Area> getAreas() {
         return areaArray;
     }
 
+    /** Method for getting list of area perimeters
+     *
+     * @param id number of area
+     * @return array of area perimeters
+     */
     public ArrayList<Pixel> getAreaPerimeter(int id) {
         if (areaArray.size() > 0)
             return areaArray.get(id).getAreaPerimeter();
         else return null;
     }
 
-    public double getMaxDensity() {//поиск максимальной плотности D
+    /** Method for searching max value of density D
+     *
+     * @return max value of density
+     */
+    public double getMaxDensity() {
         double maxDensity = 1.0;
         for (int a = 0; a < getLines().size(); a++) {
             if (getLines().get(a).getMaxDensity(backgroundIntensity) > maxDensity) {
@@ -82,35 +138,64 @@ public class MainController {
         return maxDensity;
     }
 
-    public void setBackgroundIntensity(Pixel p) {   //для установки по желанию интенсивности пикселя за интенсивность фона
+    /** Method for setting pixel intensity for background intensity by will of user
+     *
+     * @param p value of pixel
+     */
+    public void setBackgroundIntensity(Pixel p) {
         backgroundIntensityOld = backgroundIntensity;
         backgroundIntensity = p.getIntensity();
         updateData();
     }
 
+    /** Method for setting background intensity
+     *
+     * @param intensity new intensity
+     */
     public void setBackgroundIntensity(double intensity) {
         backgroundIntensityOld = backgroundIntensity;
         backgroundIntensity = intensity;
         updateData();
     }
 
-    public ArrayList<Pixel> getGraphics(int graphHeight, int height, int xBorder, int yBorder) {//на вход  идут размеры холста графика
-        //для экономии памяти проверяю, изменилось ли количество линий
+    /** Method for getting graphics.
+     * Using sizes of graphic canvas.
+     * Check change of numbers of lines for memory saving.
+     *
+     * @param graphHeight height of graphic
+     * @param height height
+     * @param xBorder border for x
+     * @param yBorder border for y
+     * @return graphic
+     */
+    public ArrayList<Pixel> getGraphics(int graphHeight, int height, int xBorder, int yBorder) {
         if ((graphPixels == null) || (numberOfLinesOld != lineArray.size()) || (backgroundIntensity != backgroundIntensityOld))
             graphPixels = getNewGraphics(graphHeight, height, xBorder, yBorder);
         backgroundIntensityOld = backgroundIntensity;
         return graphPixels;
     }
 
+    /** Method for getting table
+     *
+     * @return model of table
+     */
     public RtModel getTable() {
         return rtModel;
     }
 
+    /** Method for removing areas.
+     * Delete areas from array of areas.
+     *
+     */
     public void removeAreas() {
         areaArray.clear();
         updateData();
     }
 
+    /** Method for removing areas by using their id
+     *
+     * @param id of area
+     */
     public void removeArea(int id) {
         for (int i = 0; i < areaArray.size(); i++)
             if (areaArray.get(i).getId() == id) {
@@ -121,13 +206,11 @@ public class MainController {
         updateData();
     }
 
-    //
-    // методы из контроллера таблиц (переделано)
-    //
-    //метод для обновления данных в таблице
 
+    /** Method for updating data in table
+     *
+     */
     public void updateData() {
-        //rtModel=new RtModel();
         rtModel.removeRows();
         Double[] newData;
         for (int n = 0; n < areaArray.size(); n++) {
@@ -139,13 +222,28 @@ public class MainController {
         }
         rtModel.fireTableDataChanged();
     }
-    //метод для передачи индекса удаляемой ячейки
 
+    /** Method for transfer index of deleted row of table
+     *
+     * @param idRow id of row
+     */
     public void transferDeleteData(int idRow) {
-        //rtModel.deleteValueAt(idRow);
     }
 
-    private ArrayList<Pixel> getNewGraphics(int graphHeight, int height, int xBorder, int yBorder) {//на вход  идут размеры холста графика
+    /** Method for drawing new graphics.
+     * Using sizes of graphic canvas.
+     * Getting values of intensity.
+     * Ration of graphic.
+     * If the ordinate makes a big difference - just draw a line from point to point.
+     *
+     *
+     * @param graphHeight height of graphic
+     * @param height height
+     * @param xBorder border for x
+     * @param yBorder border for y
+     * @return drawn graphics
+     */
+    private ArrayList<Pixel> getNewGraphics(int graphHeight, int height, int xBorder, int yBorder) {
         numberOfLinesOld = lineArray.size();
         graphPixels.clear();
         for (int t = 0; t < lineArray.size(); t++) {
@@ -154,7 +252,7 @@ public class MainController {
             int x = xBorder, y, xPast, yPast;
             pixelIntensity = getLines().get(t).getPixelIntensity(0);
             if (pixelIntensity > 1)
-                y = height - yBorder - (int) ((graphHeight * Math.log10(getBackgroundIntensity() / pixelIntensity)) / (getMaxDensity()));//нормирование
+                y = height - yBorder - (int) ((graphHeight * Math.log10(getBackgroundIntensity() / pixelIntensity)) / (getMaxDensity()));
             else y = yBorder;
             if (y > height - yBorder) y = height - yBorder;
             yPast = y;
@@ -164,10 +262,9 @@ public class MainController {
             p.setColor(c);
             graphPixels.add(p);
             for (int i = 1; i < getLines().get(t).getLength(); i++) {
-                if (Math.abs(y - yPast) > 1) { //если по ординате большая разница - просто отрисовка прямой от точки к точке
+                if (Math.abs(y - yPast) > 1) {
                     for (int j = 1; j < Math.abs(y - yPast); j++) {
                         if (yPast < y) {
-                            //g.drawLine(i - 1 + xBorder, yPast + j, i - 1 + 50, yPast + j);
                             p = new Pixel();
                             p.setX(i - 1 + xBorder);
                             p.setY(yPast + j);
@@ -201,6 +298,10 @@ public class MainController {
         return graphPixels;
     }
 
+    /** Method for setting pixel intensity for background intensity by will of user
+     *
+     * @return new background intensity
+     */
     private double getBackgroundIntensity() {   //для установки по желанию интенсивности пикселя за интенсивность фона
         return backgroundIntensity;
     }
