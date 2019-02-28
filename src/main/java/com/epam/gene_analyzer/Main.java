1bc55b38da1cf82fb1371cb3e7c7c24ef8b98590
alisagareeva19 <alisagareeva19@gmail.com>
2019-02-28 09:01:29 +0300

    Add JavaDoc to main

--- src/main/java/com/epam/gene_analyzer/Main.java
+++ src/main/java/com/epam/gene_analyzer/Main.java
@@ -2,7 +2,14 @@ package com.epam.gene_analyzer;
 
 import com.epam.gene_analyzer.windows.MainWindow;
 
+/** Class for running tha project
+ *
+ */
 public class Main {
+    /** Calling main window
+     *
+     * @param args args
+     */
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
         mainWindow.showWindow();
